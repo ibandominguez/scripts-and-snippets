6d28dcb5665218548bd689bae3ce162d1881e7ca
ibandominguez <ibandominguez@live.com>
2015-04-12 23:31:50 +0100

    provision sh

---
+++ provision.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# packages
+
+brewPackages=(php56 mcrypt php56-mcrypt mysql postgresql mongodb git brew-cask homebrew/php/composer)
+casksPackages=(google-chrome node firefox virtualbox vagrant vagrant-manager filezilla atom sublime-text skype sequel-pro) 
+composerPackages=(laravel/installer laravel/homestead phpunit/phpunit codeception/codeception)
+npmPackages=(grunt-cli gulp bower nodemon sails phonegap cordova jade express)
+
+# install homebrew
+
+echo "=> installing homebrew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor && brew update && brew upgrade
+brew tap caskroom/homebrew-cask
+brew tap homebrew/php
+
+# brewPackages
+
+echo "=> installing brewPackages"
+for i in "${brewPackages[@]}"
+	do
+	  brew install $i
+	done
+
+# casksPackages
+
+echo "=> installing casksPackages"
+for i in "${casksPackages[@]}"
+	do
+	  brew cask install $i
+	done
+
+# composerPackages
+
+echo "=> installing composerPackages"
+for i in "${composerPackages[@]}"
+	do
+	  sudo composer global require $i
+	done
+
+# npmPackages
+
+echo "=> installing npmPackages"
+for i in "${npmPackages[@]}"
+	do
+	  sudo npm install $i -g
+	done
+
