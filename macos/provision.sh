bdd08a338f1368e60637865f49ed3db8864bbd8d
ibandominguez <ibandominguez@live.com>
2016-05-19 21:53:50 +0100

    add graphicsmagick and imagemagick to brew

--- macos/provision.sh
+++ macos/provision.sh
@@ -9,7 +9,7 @@ fi
 
 # packages
 
-brewPackages=(php56 mcrypt php56-mcrypt mysql redis postgresql mongodb git brew-cask homebrew/php/composer nginx homebrew/apache/httpd24 tree curl android-sdk python install sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer)
+brewPackages=(php56 mcrypt php56-mcrypt mysql redis imagemagick graphicsmagick postgresql mongodb git brew-cask homebrew/php/composer nginx homebrew/apache/httpd24 tree curl android-sdk python install sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer)
 casksPackages=(google-chrome node firefox virtualbox vagrant vagrant-manager filezilla atom skype sequel-pro arduino)
 composerPackages=(laravel/installer laravel/homestead phpunit/phpunit codeception/codeception squizlabs/php_codesniffer)
 npmPackages=(grunt-cli gulp bower nodemon sails protactor phantomjs forever phonegap cordova jade express)
