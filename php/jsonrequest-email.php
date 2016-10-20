e595eb9ec99e92321d36f2a675cb82d7ad1ca663
Iban Dominguez <ibandominguez@live.com>
2016-10-20 15:55:52 +0100

    Create jsonrequest-email.php

---
+++ php/jsonrequest-email.php
@@ -0,0 +1,20 @@
+<?php
+
+header('Content-Type: application/json');
+
+$input   = file_get_contents('php://input');
+$params  = (object) json_decode($input, true);
+
+$to      = '<valid-email>';
+$subect  = '<the-subject>';
+
+if (isset($params->email) && filter_var($params->email, FILTER_VALIDATE_EMAIL)):
+  $from    = $params->email;
+  $headers = "From: $from\r\nReply-To: $from\r\nX-Mailer: PHP/" . phpversion();
+  $status  = mail($to, $subect, $input, $headers);
+
+  echo json_encode(array('status' => $status));
+else:
+  header('Bad Request', true, 400);
+  echo json_encode(array('error' => 'A valid email is required'));
+endif;
