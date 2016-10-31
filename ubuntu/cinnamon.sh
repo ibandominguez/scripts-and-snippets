9a44e3796f355e1381cc12fa1ec0532f138db1bd
Iban Dominguez <ibandominguez@live.com>
2016-10-31 16:59:03 +0000

    Create cinnamon.sh

---
+++ ubuntu/cinnamon.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This script will install the cinnamon desktop
+# Logout and login, note that you'll have a little icon next to the login prompt with
+# a set of choices for the desktop environment
+
+sudo add-apt-repository ppa:gwendal-lebihan-dev/cinnamon-nightly
+sudo apt-get update
+sudo apt-get install cinnamon
