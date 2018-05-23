389b57d5c615ad6943f69a871e60b96275c7df51
Iban Dominguez <ibandominguez@live.com>
2018-05-23 04:16:38 +0100

    Update file-organizer.js

--- node/file-organizer.js
+++ node/file-organizer.js
@@ -4,10 +4,28 @@ const fs = require('fs')
 const path = require('path')
 
 /**
- * Find all files inside a dir, recursively.
- * @function getAllFiles
- * @param  {string} dir Dir path string.
- * @return {string[]} Array with all file names that are inside the directory.
+ * @param  {Date} date
+ * @return {string}
+ */
+const getFormattedDate = (date) => {
+  let dd = date.getDate()
+  let mm = date.getMonth() + 1
+  let yyyy = date.getFullYear()
+
+  if (dd < 10) {
+    dd = `0${dd}`
+  }
+
+  if (mm < 10) {
+    mm = `0${mm}`
+  }
+
+  return `${dd}-${mm}-${yyyy}`
+}
+
+/**
+ * @param  {string}
+ * @return {string[]}
  */
 const getAllFiles = dir => {
   return fs.readdirSync(dir).reduce((files, file) => {
@@ -16,3 +34,21 @@ const getAllFiles = dir => {
     return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
   }, []);
 }
+
+/**
+ * Use Example
+ */
+
+const SOURCE_DIRECTORY = '/Users/ibandominguez/Desktop/'
+const allFiles = getAllFiles(SOURCE_DIRECTORY)
+
+allFiles.map((file) => {
+  const fileData = fs.statSync(file)
+  const destinationPath = path.join(SOURCE_DIRECTORY, getFormattedDate(fileData.birthtime))
+
+  if (!fs.existsSync(destinationPath)) {
+    fs.mkdirSync(destinationPath)
+  }
+
+  fs.copyFileSync(file, path.join(destinationPath, path.basename(file)))
+})
