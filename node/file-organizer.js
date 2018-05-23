44e4fc0552ae9ac97d1751646652dd46a5910c3d
Iban Dominguez <ibandominguez@live.com>
2018-05-23 03:37:36 +0100

    Create file-organizer.js

---
+++ node/file-organizer.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+/**
+ * Find all files inside a dir, recursively.
+ * @function getAllFiles
+ * @param  {string} dir Dir path string.
+ * @return {string[]} Array with all file names that are inside the directory.
+ */
+const getAllFiles = dir => {
+  return fs.readdirSync(dir).reduce((files, file) => {
+    const name = path.join(dir, file);
+    const isDirectory = fs.statSync(name).isDirectory();
+    return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+  }, []);
+}
