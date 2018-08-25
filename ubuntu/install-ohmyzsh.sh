081c6e4d72423f08e5e014e8de3cb42792a5541a
Iban Dominguez <domingueznoda@gmail.com>
2018-08-25 03:48:56 +0100

    Update install-ohmyzsh.sh

--- ubuntu/install-ohmyzsh.sh
+++ ubuntu/install-ohmyzsh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Install ZSH
-sudo apt-get install zsh
+sudo apt-get install zsh -y
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 chsh -s `which zsh`
