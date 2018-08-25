d4eba54767a692f21603c8220fa313ffc6e167fb
Iban Dominguez <domingueznoda@gmail.com>
2018-08-25 06:37:36 +0100

    Create install-numix.sh

---
+++ ubuntu/install-numix.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install numix-gtk-theme numix-icon-theme-circle
+
+# (Uncomment) Install Wallpappers
+# sudo apt-get install numix-wallpaper-*
+
+# Install tweak-tool
+# (If ussing unity) sudo apt-get install unity-tweak-tool
+sudo apt-get install gnome-tweak-tool
