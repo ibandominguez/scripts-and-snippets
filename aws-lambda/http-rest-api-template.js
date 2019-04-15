fb0709d6386842918831cfd3e8fe9536e4325b56
Iban Dominguez <domingueznoda@gmail.com>
2019-04-15 05:09:11 +0100

    Update http-rest-api-template.js

--- aws-lambda/http-rest-api-template.js
+++ aws-lambda/http-rest-api-template.js
@@ -1,27 +1,32 @@
 'use strict'
 
 exports.handler = async (event) => {
-  // event.headers : Object
-  // event.queryStringParameters : Object (null if empty)
-  // event.body : String (null if empty)
+  let request = {
+    method: event.httpMethod,
+    headers: event.headers,
+    params: event.queryStringParameters,
+    body: event.body
+  }
 
   let response = {
     statusCode: 200,
     headers: {
-      ...event.headers,
+      ...request.headers,
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
       'Content-type': 'application/json'
     }
   }
 
+  // Parse request body
   try {
-    event.body = JSON.parse(event.body)
+    request.body = JSON.parse(event.body)
   } catch (error) {
+    response.statusCode = 500
     response.body = JSON.stringify({ error })
-    return response
   }
 
-  response.body = JSON.stringify(event)
+  response.body = JSON.stringify({ request })
+
   return response
 }
