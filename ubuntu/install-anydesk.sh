c9d754320ec5a5f2d80f0c9b72772122b36aee45
Iban Dominguez <domingueznoda@gmail.com>
2024-04-28 21:25:22 +0100

    Create install-anydesk.sh

---
+++ ubuntu/install-anydesk.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt update
+sudo apt install anydesk
