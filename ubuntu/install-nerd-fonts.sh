4b8bc4949195ca2b37d34dde728d342b1db740fd
Ibán Domínguez <domingueznoda@gmail.com>
2024-04-30 18:26:27 +0100

    Updated nerd fonts version and download script

--- ubuntu/install-nerd-fonts.sh
+++ ubuntu/install-nerd-fonts.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
 declare -a fonts=(
-    BitstreamVeraSansMono
-    CodeNewRoman
-    DroidSansMono
-    FiraCode
-    FiraMono
-    Go-Mono
-    Hack
-    Hermit
-    JetBrainsMono
-    Meslo
-    Noto
-    Overpass
-    ProggyClean
-    RobotoMono
-    SourceCodePro
-    SpaceMono
-    Ubuntu
-    UbuntuMono
+  BitstreamVeraSansMono
+  CodeNewRoman
+  DroidSansMono
+  FiraCode
+  FiraMono
+  Go-Mono
+  Hack
+  Hermit
+  JetBrainsMono
+  Meslo
+  Noto
+  Overpass
+  ProggyClean
+  RobotoMono
+  SourceCodePro
+  SpaceMono
 )
 
-version='2.1.0'
+version='3.2.1'
 fonts_dir="${HOME}/.local/share/fonts"
 
 if [[ ! -d "$fonts_dir" ]]; then
-    mkdir -p "$fonts_dir"
+  mkdir -p "$fonts_dir"
 fi
 
 for font in "${fonts[@]}"; do
-    zip_file="${font}.zip"
-    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-    echo "Downloading $download_url"
-    wget "$download_url"
-    unzip "$zip_file" -d "$fonts_dir"
-    rm "$zip_file"
+  zip_file="${font}.zip"
+  download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+  echo "Downloading $download_url"
+  wget "$download_url"
+  unzip -o "$zip_file" -d "$fonts_dir"
+  rm "$zip_file"
 done
 
 find "$fonts_dir" -name '*Windows Compatible*' -delete
